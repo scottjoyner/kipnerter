2ba6ef8e234793b472c272b0cff65a103efc787c
blksc <blksc@DESKTOP-Q051IJJ>
2020-05-11 04:30:37 -0400

    Added Files

--- kipnerter/src/main/java/ims/scjoyner/inc/kipnerter/controllers/DefaultController.java
+++ kipnerter/src/main/java/ims/scjoyner/inc/kipnerter/controllers/DefaultController.java
@@ -2,6 +2,7 @@ package ims.scjoyner.inc.kipnerter.controllers;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,19 +14,31 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class DefaultController {
 
+//    /**
+//     * This controller is used to redirect the authenticated user to the
+//     * appropriate landing screen based on their role.
+//     *
+//     * @param model
+//     *            The data from the front end
+//     * @return The page to be displayed to the user
+//     */
+//    @RequestMapping ( value = "/" )
+//    public RedirectView index ( final Model model ) {
+//        final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        final List< ? extends GrantedAuthority> auths = (List< ? extends GrantedAuthority>) auth.getAuthorities();
+//        final GrantedAuthority ga = auths.get( 0 );
+//        return new RedirectView( ims.scjoyner.inc.kipnerter.models.enums.Role.valueOf( ga.toString() ).getLanding() );
+//    }
+//    
     /**
-     * This controller is used to redirect the authenticated user to the
-     * appropriate landing screen based on their role.
+     * Add or delete user
      *
      * @param model
-     *            The data from the front end
-     * @return The page to be displayed to the user
+     *            data for front end
+     * @return mapping
      */
     @RequestMapping ( value = "/" )
-    public RedirectView index ( final Model model ) {
-        final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        final List< ? extends GrantedAuthority> auths = (List< ? extends GrantedAuthority>) auth.getAuthorities();
-        final GrantedAuthority ga = auths.get( 0 );
-        return new RedirectView( ims.scjoyner.inc.kipnerter.models.enums.Role.valueOf( ga.toString() ).getLanding() );
+    public String manageUser ( final Model model ) {
+        return "/home";
     }
 }
